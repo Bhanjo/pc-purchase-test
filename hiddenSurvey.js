a20ad39b923bf6857f4f43b3a0008dd65ccbd245
Bhanjo <hanjo1515@naver.com>
2020-12-15 17:38:13 +0900

    201215

---
+++ hiddenSurvey.js
@@ -0,0 +1,33 @@
+let index = 1;
+const submitBtn = document.getElementsByClassName("submitBtn");
+const preBtn = document.getElementsByClassName("previousBtn");
+const nextBtn = document.getElementsByClassName("nextBtn");
+const x = document.getElementsByClassName("questionInner");
+
+function nextSurvey() {
+    let i;
+    for(i = 0; i < x.length; i++) {
+        x[i].style.display="none";
+    }
+    index++;
+    x[index-1].style.display = "block";
+    preBtn[0].style.display="inline";
+    if(index == x.length) {
+        submitBtn[0].style.display="block";
+        preBtn[0].style.display="none";
+        nextBtn[0].style.display="none";
+    }
+}
+
+function previousSurvey() {
+    let i;
+    for(i = 0; i < x.length; i++) {
+        x[i].style.display="none";
+    }
+    index--;
+    x[index-1].style.display = "block";
+    if(index == 1) {
+        preBtn[0].style.display="none";
+        console.log("시작");
+    }
+}
